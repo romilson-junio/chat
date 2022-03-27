cb31b0ad150c4447930ee8a4f9f7d28a41d53c37
Romilson Junio <Junio.romilson@gmail.com>
2022-03-26 23:51:55 -0300

    Change client error theme selection

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 public class Client extends Thread {
 
@@ -51,17 +52,6 @@ public class Client extends Thread {
 		System.out.println("Informe o nome do cliente:");
 		nome_cliente = teclado.readLine();
 
-		// Cria o socket de acesso ao server hostname na porta 8567
-		Socket cliente = new Socket("192.168.0.9", 8567);
-		System.out.println(nome_cliente + " entrou no chat!");
-
-		// Cria os strems de saída com o servidor
-		DataOutputStream saida_servidor = new DataOutputStream(cliente.getOutputStream());
-		
-		//Instancia uma nova threads
-		Thread t = new Client(cliente);
-		t.start();
-
 		System.out.println("Escolha um tema: \n"
 				+ "[ 1 ] Entretenimento" + "\n"
 				+ "[ 2 ] Economia" + "\n"
@@ -69,18 +59,30 @@ public class Client extends Thread {
 				+ "[ 4 ] Fim");
 		// Recebi o assunto selecionado
 		msg_digitada = teclado.readLine();
-		saida_servidor.writeBytes(msg_digitada + "\n");
-		
-		while(true) {
-			// Envia para o servidor o nome do cliente
-			saida_servidor.writeBytes(nome_cliente.concat("\n"));
+		if(Arrays.asList("1", "2", "3").contains(msg_digitada)) {
+			// Cria o socket de acesso ao server hostname na porta 8567
+			Socket cliente = new Socket("localhost", 8567);
+
+			// Cria os strems de saída com o servidor
+			DataOutputStream saida_servidor = new DataOutputStream(cliente.getOutputStream());
 			
-			// Receber mensagem do cliente
-			msg_digitada = teclado.readLine();
-			saida_servidor.writeBytes(msg_digitada.concat("\n"));
+			//Instancia uma nova threads
+			Thread t = new Client(cliente);
+			t.start();
 			
+			saida_servidor.writeBytes(msg_digitada + "\n");
+			
+			while(true) {
+				// Envia para o servidor o nome do cliente
+				saida_servidor.writeBytes(nome_cliente.concat("\n"));
+				
+				// Receber mensagem do cliente
+				msg_digitada = teclado.readLine();
+				saida_servidor.writeBytes(msg_digitada.concat("\n"));
+				
+			}	
 		}
-		
+		System.out.println("FIM!");
 	}
 
 }
